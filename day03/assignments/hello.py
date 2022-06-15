be2fae7d76e7798f06aab29a44dc522d13246d62
afkhkharzaineb <zaineb.afkhkhar@um6p.ma>
2022-06-15 11:36:03 +0100

    hello.py

--- day03/assignments/hello.py
+++ day03/assignments/hello.py
@@ -1,6 +1,9 @@
-# write your program here
-from pyccel.stdlib.internal.openmp import omp_get_thread_num, omp_get_num_threads
-def hello():
-   for i in range(omp_get_num_threads()) :
-       print("hello from the rank "+str(omp_get_thread_num())+" thread")
-hello()
+import pyccel
+if __name__ == "__main__" :
+    from pyccel.stdlib.internal.openmp import omp_in_parallel,omp_get_thread_num, omp_get_num_threads
+    #$omp parallel
+    n = omp_get_num_threads()
+    for i in range(n) :
+        p = omp_get_thread_num()
+        print("hello from the rank ",p," thread")
+    #$omp end parallel
